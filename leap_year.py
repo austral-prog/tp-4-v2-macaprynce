567ff3d33b095672bc069851192e137248360440
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-08-27 17:02:59 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,10 @@
 def leap_year():
     year = int(input("Ingrese un año:"))
-if year%400== 0:
-  print("El año ingresado es bisiesto")
-elif year%4== 0 and year%100!=0:
-  print("El año ingresado es bisiesto")
-else:
-  print("El año ingresado no es bisiesto")
+    if year%400== 0:
+      print("El año ingresado es bisiesto")
+    elif year%4== 0 and year%100!=0:
+      print("El año ingresado es bisiesto")
+    else:
+      print("El año ingresado no es bisiesto")
 
    
